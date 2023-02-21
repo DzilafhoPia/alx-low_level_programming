ae4fbb156096ccafdbdb363a98e518d3531c8259
DzilafhoPia <dzilafhopia17@gmail.com>
2023-02-21 15:20:42 -0800

    Nested loops

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,10 +9,10 @@ void times_table(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j< 10; j++)
+		for (j = 0; j < 10; j++)
 		{
 			k = j * i;
-			if (j ==0)
+			if (j == 0)
 			{
 				_putchar(k + '0');
 			}
@@ -23,7 +23,7 @@ void times_table(void)
 				_putchar(' ');
 				_putchar(' ');
 				_putchar(k + '0');
-			}else if (k >= 10)
+			} else if (k >= 10)
 			{
 				_putchar(',');
 				_putchar(' ');

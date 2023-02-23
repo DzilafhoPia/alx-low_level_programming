450ef6f4756c4d146757718f9531aa5ef3694e96
DzilafhoPia <dzilafhopia17@gmail.com>
2023-02-23 05:00:57 -0800

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -11,15 +11,16 @@ void print_most_numbers(void)
 {
 	int x = 0;
 
-	for (x <= 9; x++)
+	for (; x <= 9; x++)
 	{
 	if (x == 2 || x == 4)
 	{
-	continue}
+	continue;
 	}
 	else
 	{
 	_putchar(x + '0');
+	}
 	}
 	_putchar('\n');
 }

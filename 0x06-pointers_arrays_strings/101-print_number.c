99fb795027e883258fd9d48d1a766c4f384d01cd
DzilafhoPia <dzilafhopia17@gmail.com>
2023-03-02 15:45:18 -0800

    102-magic.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * print_number - print numbers chars
- * @n: integer params
- * Return: 0
+ * print_number - prints an integer.
+ * @n: The integer to be printed.
+ *
  */
 
 void print_number(int n)
@@ -12,10 +12,10 @@ void print_number(int n)
 
 	if (n < 0)
 	{
-	_putchar('-');
+	putchar('-');
 	num = -num;
 	}
 	if ((num / 10) > 0)
 	print_number(num / 10);
-	_putchar((num % 10) + '0');
+	putchar((num % 10) + '0');
 }

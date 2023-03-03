ca12de74878d99498e5a212fedf90b6b9cad2df6
DzilafhoPia <dzilafhopia17@gmail.com>
2023-03-02 17:00:52 -0800

    104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -13,6 +13,7 @@ void print_buffer(char *b, int size)
 	int o, j, i;
 
 	o = 0;
+
 	if (size <= 0)
 	{
 		printf("\n");
@@ -35,7 +36,7 @@ void print_buffer(char *b, int size)
 		}
 		for (i = 0; i < j; i++)
 		{
-			int c = *(b + 0 + i);
+			int c = *(b + o + i);
 
 			if (c < 32 || c > 132)
 			{
